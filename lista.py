91e2e66bb3710eda55ecca1e90f3509ff4928a9f
Igorx945 <128699017+Igorx945@users.noreply.github.com>
2024-04-02 11:27:49 -0300

    Add files via upload

--- lista.py
+++ lista.py
@@ -1,41 +1,55 @@
-lista = []
-while True:
-    print("Opções:")
-    print("1. Adicionar um item à lista")
-    print("2. Remover um item da lista")
-    print("3. Exibir a lista")
-    print("4. Gravar lista")
-    print("5. Sair")
+import os
 
-    escolha = int(input("Digite sua escolha (1-5): "))
+def mostrarLista(lista):
+    print("Lista atual:", lista)
 
-    if escolha == 1:
-        novo_item = input("Digite o item a ser adicionado: ")
-        lista.append(novo_item)
-        print("Item adicionado!")
+def lista():
+    lista = []
+    while True:
+        print(" Escolha uma opção:")
+        print("1 - Adicionar item na lista")
+        print("2 - Excluir um item da lista")
+        print("3 - Mostrar lista")
+        print("4 - Gravar lista")
+        print("5 - Ler um arquivo de lista epecifico")
+        print("6 - Sair")
 
-    elif escolha == 2:
-        if len(lista) > 0:
-            remove_item = input("Digite o item a ser removido: ")
-            if remove_item in lista:
-                lista.remove(remove_item)
-                print("Item removido!")
+        opc = input("Digite a opção desejada: ")
+
+        if opc == '1':
+            novo_item = input("Digite um novo item para adicionar na lista: ")
+            lista.append(novo_item)
+        elif opc == '2':
+            itemExcluir = input("Digite o item a ser excluído da lista: ")
+            if itemExcluir in lista:
+                lista.remove(itemExcluir)
             else:
-                print("Item não encontrado na lista.")
+                print("O item não está na lista.")
+        elif opc == '3':
+            mostrarLista(lista)
+        elif opc == '4':
+            arquivo = input("Digite o nome do arquivo a ser gravado: ")
+            try:
+                with open(arquivo, 'w') as f:
+                    f.write(str(lista))
+                print("Lista gravada com sucesso no arquivo", arquivo)
+            except Exception as e:
+                print("Erro ao gravar o arquivo:", e)
+        elif opc == '5':
+            arquivos = os.listdir()
+            print("Lista de Arquivos disponíveis:")
+            for arquivo in arquivos:
+                print(arquivo)
+            arquivoEscolhido = input("Digite o nome do arquivo para ler: ")
+            try:
+                with open(arquivoEscolhido, 'r') as f:
+                    lista = eval(f.read())
+                    mostrarLista(lista)
+            except Exception as e:
+                print("Erro ao ler o arquivo:", e)
+        elif opc == '6':
+            print("Saindo do programa...")
+            break
         else:
-            print("Lista vazia.")
-
-    elif escolha == 3:
-        print("Lista atual:")
-        print(lista)
-    elif escolha == 4:
-        nome_arq = input("Digite o nome do arquivo")
-        with open(nome_arq,"w") as arquivo:
-            for item in lista:
-                arquivo.write(item+"\n")
-        print("Arquivo gravado com sucesso!",nome_arq)
-    elif escolha == 5:
-        print("Até mais!")
-        break
-    else:
-        print("Escolha inválida. Por favor, tente novamente.")
+            print("Opção inválida.")
+lista()
